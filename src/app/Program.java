e805f9a8b0e96dc333f76bbba61181097379351c
GabrielSz2 <gsouzaribeiro21@gmail.com>
2024-07-25 08:41:28 -0400

    SellerDaoJDBC and DaoFactory

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		
 		Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.00, obj);
 		System.out.println(sl);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
 	}

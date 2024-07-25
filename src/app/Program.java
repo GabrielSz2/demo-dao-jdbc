bdb49edb3a2474a012fbe20bd0f2d6fe2254ec4c
GabrielSz2 <gsouzaribeiro21@gmail.com>
2024-07-25 08:28:48 -0400

    Seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,6 +1,9 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1, "books");
 		System.out.println(obj);
+		
+		Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.00, obj);
+		System.out.println(sl);
 		
 		
 	}
